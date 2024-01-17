d7a67660c08c2cf96dbfde5a5dfbd8657cc9fc49
Ax-SylvesterHommuk <ax-sylvester.hommuk@voco.ee>
2024-01-17 10:10:54 +0200

    Update page

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <LandingPage />
   <SecondPage />
+  <ThirdPage />
 </template>
 
 <script>
 import LandingPage from './components/desktop/LandingPage.vue';
 import SecondPage from './components/desktop/SecondPage.vue';
+import ThirdPage from './components/desktop/ThirdPage.vue';
 
 export default {
   components: {
     LandingPage,
     SecondPage,
+    ThirdPage,
   },
 };
 </script>
