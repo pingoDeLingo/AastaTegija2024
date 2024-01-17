d934472eb24c12060feec79465d98fc9e5e41672
Ax-SylvesterHommuk <ax-sylvester.hommuk@voco.ee>
2024-01-17 10:03:17 +0200

    Update page

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <LandingPage />
+  <SecondPage />
 </template>
 
 <script>
-import LandingPage from './components/LandingPage.vue';
+import LandingPage from './components/desktop/LandingPage.vue';
+import SecondPage from './components/desktop/SecondPage.vue';
 
 export default {
   components: {
     LandingPage,
+    SecondPage,
   },
 };
 </script>
